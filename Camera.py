89206749ec6814802bdf8b2b30efbe55ff63f435
Tayler Caufield <taylercaufield@gmail.com>
2026-05-12 09:13:19 -0700

    Updated camera file with proper imports. Added tag to generate stream onto the html file

--- Camera.py
+++ Camera.py
@@ -2,48 +2,42 @@
 ## Description: This file handles the camera video stream functionality. 
 
 from threading import Thread 
+from flask import Flask, Response
+from picamera2 import Picamera2
 import cv2
 
-class Video: 
-    def __init__(self, resolution=(640,480), framerate=30, src=0): 
-          
-    # import pi camera packages
-     from picamera.array import PiRGBArray
-     from picamera import PiCamera
+app = Flask(__name__)
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"size": (640, 480)}))
+picam2.start()
 
-     self.camera = PiCamera()
-     self.camera.resolution = resolution
-     self.camera.framerate = framerate
-     self.rawCapture = PiRGBArray(self.camera, size=self.resolution)
-     self.stream = self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port = True)
+# Function to generate frames from the camera stream
+# Buffer compresses the raw Numpy array into a JPEG format for streaming.
+# yield pauses the data send the n resumes on the next loop iteration, this is what makes 
+# it a stream.
 
-     self.frame =[]
+def generate_frames():
+    while True:
+        frame = picam2.capture_array() 
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+        
+@app.route('\stream')
+def index():
+    return Response(generate_frames(), 
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/')
+def index():
+    return '<img src="/stream" width="640" height="480">'
 
-     self.stopped = False
 
-     def start(self):
-        Thread(target=self.update,args=()).start()
-        return self 
-     
-     def update(self):
-      # for loop that will keep looping until the thread is stopped. 
-      # Loop grabs the frame from the active stream.
-        for f in self.streamn: 
-           self.frame = f.array
-           self.rawCapture.truncate(0)
-        if self.stopped:
-               self.stream.close()
-               self.rawCapture.close()
-               self.camera.close()
-               return
-        (self.grabbed, self.frame) = self.stream.read()
+# Starts the flask web server. this host controls who can connect to the sever. 
+# 0.0.0.0 listens on all available interfaces, allowing connections from any IP address.
+app.run(host='0.0.0.0', port=5000, debug=True)
 
-# Method to return the most recent frame
-def read(self):
-         return self.frame 
-# Method to stop the thread and camera 
-def stop(self):
-         self.stopped = True
+
 
     
 
